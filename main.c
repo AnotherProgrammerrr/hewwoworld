9a3bdaf717903bfe2c74f2a9c5e83683bab82e24
AnotherProgrammerrr <76083714+AnotherProgrammerrr@users.noreply.github.com>
2023-07-10 12:58:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(){
     
     int numOfColors = sizeof(rainbowColors) / sizeof(rainbowColors[0]);
 
-    while(0 < 1 < 2 < 3 < 4 < 5 < 6 < 7 < 8 < 9){
+    while(0 < 1){
         for(int i = 0; i < numOfColors; i++){
             printf("%shewwo, world.\033[0m\n", rainbowColors[i]);
         };
